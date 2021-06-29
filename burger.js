0563df6b2be47e88e29503af685f5849902061dc
Vinicio Chavarría <vichavarria21@gmail.com>
2021-06-28 18:14:22 -0600

    Proyecto iniciado-mobile_first

---
+++ burger.js
@@ -0,0 +1,13 @@
+const iconMenu = document.getElementById('icon-menu');
+const menu = document.getElementById('menu');
+/* const displayNone = document.getElementById('menu').style.display = 'none'; */
+
+iconMenu.addEventListener('click', (bodyClick) => {
+    menu.classList.toggle('active');
+    document.body.classList.toggle('opacity');
+})
+
+
+
+
+
