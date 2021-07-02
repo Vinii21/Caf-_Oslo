c161f83748e7274d26e5504b1f0114695f819c41
Vinicio Chavarría <vichavarria21@gmail.com>
2021-07-02 11:06:31 -0600

    Añadi la clase none al menu para que desaparezca, y en burger.js arregle el códdigo de la función que hace que se despliegue el menú para mejorarlo en dispositivos moviles

--- burger.js
+++ burger.js
@@ -3,6 +3,7 @@ const menu = document.getElementById('menu');
 /* const displayNone = document.getElementById('menu').style.display = 'none'; */
 
 iconMenu.addEventListener('click', (bodyClick) => {
+    menu.classList.remove('none')
     menu.classList.toggle('active');
     document.body.classList.toggle('opacity');
 })
