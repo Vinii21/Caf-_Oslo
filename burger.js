3d32808c9a6002a0e45673a852c9e6dda57807f2
Vinicio Chavarría <vichavarria21@gmail.com>
2021-07-02 11:23:21 -0600

    Mejora burger.js

--- burger.js
+++ burger.js
@@ -1,11 +1,13 @@
 const iconMenu = document.getElementById('icon-menu');
 const menu = document.getElementById('menu');
-/* const displayNone = document.getElementById('menu').style.display = 'none'; */
 
 iconMenu.addEventListener('click', (bodyClick) => {
     menu.classList.remove('none')
     menu.classList.toggle('active');
     document.body.classList.toggle('opacity');
+    iconMenu.addEventListener('click', (e) => {
+        menu.classList.toggle('none')
+    })
 })
 
 
